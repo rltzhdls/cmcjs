c2142044329e126cb9048bdcc6ca77c147597c3e
rltzhdls <136443821+rltzhdls@users.noreply.github.com>
2024-11-06 17:38:28 +0900

    Add files via upload

---
+++ cmfjrues.js
@@ -0,0 +1,9 @@
+$(document).ready(function () {
+    var vds = document.getElementById('vds');
+    
+    if (vds) {
+        vds.src = "https://vod.plaync.com/lineage/remaster0320/0320_remaster_bg3.mp4";
+        document.getElementById('vd').load();
+    }
+});
+
